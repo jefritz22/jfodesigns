0272045b4be55c2316694fcdacd6c5d76ce09b77
jefritz22 <jefritz.espino@gmail.com>
2025-06-03 14:54:02 +1200

    yes

--- js/title.js
+++ js/title.js
@@ -26,7 +26,7 @@ document.querySelectorAll('.exhibition-heading span').forEach(span => {
 
 // TYPING ANIMATION
 var typed = new Typed(".auto-type", {
-    strings: ["UX/UI design", "copywriting", "graphic design"],
+    strings: ["UX/UI design", "copywriting", "graphic design", "developer"],
     typeSpeed:50,
     backSpeed:50,
     loop: true,
