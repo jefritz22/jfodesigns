2ae2310931c22dc869a752f20007dd00dfe83b19
jefritz22 <jefritz.espino@gmail.com>
2025-06-11 11:09:15 +1200

    loading screen

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-
-
-// // NAME ANIMATION
-// gsap.to(".name", {
-// y: -20,
-// duration: 3,
-// ease: "power1.inOut",
-// yoyo: true,
-// repeat: -1
-// });
-
 //   SCROLL TRIGGER
 const boxes = document.querySelectorAll("img, p, h3, h2, section");
 
@@ -23,29 +12,6 @@ boxes.forEach( (box)=>{
 
 })
 
-// CURSOR ANIMATION
-// document.addEventListener('mousemove', (e) => {
-// const colors = ['#FA4411', '#849E00'];
-// const randomColor = colors[Math.floor(Math.random() * colors.length)];
-
-// const circle = document.createElement('div');
-// circle.classList.add('heart');
-// circle.style.left = `${e.pageX}px`;
-// circle.style.top = `${e.pageY}px`;
-// circle.style.backgroundColor = randomColor;
-
-// document.getElementById('heartContainer').appendChild(circle);
-
-// setTimeout(() => {
-//   circle.style.transform = 'translateY(-30px)';
-//   circle.style.opacity = '0';
-// }, 50);
-
-// setTimeout(() => {
-//   circle.remove();
-// }, 300);
-// });
-
 // GRADIENT CURSOR
 var cursor = document.querySelector('.blob');
 
@@ -53,4 +19,8 @@ document.addEventListener('mousemove', function(e){
   var x = e.clientX;
   var y = e.clientY;
   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+window.addEventListener('load', function () {
+    document.getElementById('loading-screen').style.display = 'none';
 });
