a03ee97dc0e47e11e89a5a164cc45774ba87f56f
jefritz22 <jefritz.espino@gmail.com>
2025-07-09 21:05:34 +1200

    bonk

--- js/title.js
+++ js/title.js
@@ -4,19 +4,19 @@ heading.innerHTML = heading.textContent.split('').map(letter => `<span>${letter}
 
 document.querySelectorAll('.exhibition-heading span').forEach(span => {
   span.addEventListener('mouseenter', () => {
-    gsap.to(span, { scale: 1.3, color: "#FA4411", duration: 0.3 });
+    gsap.to(span, { scale: 1.3, color: "#849E00", duration: 0.3 });
   });
 
   span.addEventListener('mouseleave', () => {
-    gsap.to(span, { scale: 1, color: "#FA4411", duration: 0.3 });
+    gsap.to(span, { scale: 1, color: "#849E00", duration: 0.3 });
   });
 
   let isClicked = false;
   span.addEventListener('click', () => {
     if (!isClicked) {
-      gsap.to(span, { rotation: 180, color: "#849E00", duration: 0.5, ease: "power3.out" });
+      gsap.to(span, { rotation: 180, color: "#671600", duration: 0.5, ease: "power3.out" });
     } else {
-      gsap.to(span, { rotation: 0, color: "#849E00", duration: 0.5, ease: "power3.out" });
+      gsap.to(span, { rotation: 0, color: "#671600", duration: 0.5, ease: "power3.out" });
     }
     isClicked = !isClicked;
   });
