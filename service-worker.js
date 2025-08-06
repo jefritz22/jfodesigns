8c0b4ca019e1fa6bf00cc0f6bc12f5ed21aa9b7d
jefritz22 <jefritz.espino@gmail.com>
2025-08-06 12:13:37 +1200

    capstonestuff

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const cacheName = 'teachable-machine-pwa-v1';
+const assets = [
+    './',
+    './index.html',
+    './manifest.json',
+    './capstoneappicon.jpg',
+    './capstoneappicon.jpg'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => cache.addAll(assets))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
